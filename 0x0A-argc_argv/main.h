6e195e23dfd551232214333d0314423533302db6
kiim28 <hatim.ashigaru@gmail.com>
2023-07-08 08:24:32 -0700

    ARGV

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+
+#ifndef main.h
+#define main.h
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * prototypes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+
+#endif /* main.h */
